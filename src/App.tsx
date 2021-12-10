94d0de15d67db5bf63aeb563942df1ea71743964
Miguel Lucena <dev.miguellucena@gmail.com>
2021-12-10 12:02:34 +0100

    Remove responsive container

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, {useEffect, useCallback, useState} from 'react';
 import './App.css';
 import { getAllCasesByPublishDate } from "./repo/Cases";
 import { groupBy } from "lodash"
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 function App() {
   const [ year, setYear ] = useState('2020')
@@ -25,26 +25,24 @@ function App() {
     <div className="App">
       <header className="App-header">
         {casesByDate.length > 0 &&
-          <ResponsiveContainer width="100%" height="100%">
-            <BarChart
-                width={500}
-                height={300}
-                data={casesByDate}
-                margin={{
-                  top: 5,
-                  right: 30,
-                  left: 20,
-                  bottom: 5,
-                }}
-            >
-              <CartesianGrid strokeDasharray="3 3"/>
-              <XAxis dataKey="month"/>
-              <YAxis/>
-              <Tooltip/>
-              <Legend/>
-              <Bar dataKey="cases" fill="#8884d8"/>
-            </BarChart>
-          </ResponsiveContainer>
+          <BarChart
+              width={1700}
+              height={600}
+              data={casesByDate}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+          >
+            <CartesianGrid strokeDasharray="3 3"/>
+            <XAxis dataKey="month"/>
+            <YAxis/>
+            <Tooltip/>
+            <Legend/>
+            <Bar dataKey="cases" fill="#8884d8"/>
+          </BarChart>
         }
       </header>
     </div>
