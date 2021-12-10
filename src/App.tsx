14dc17e6cf42c487969e6ef362e6dec821418b06
Miguel Lucena <dev.miguellucena@gmail.com>
2021-12-10 11:56:47 +0100

    Add chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React, {useEffect, useCallback, useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { getAllCasesByPublishDate } from "./repo/Cases";
 import { groupBy } from "lodash"
-import {CasesByPublishDate} from "./model/Api";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 function App() {
   const [ year, setYear ] = useState('2020')
-  const [ casesByDate, setCasesByDate ] = useState<CasesByPublishDate[]>([])
+  const [ casesByDate, setCasesByDate ] = useState<{ month: string, cases: number }[]>([])
 
   const fetchApi = useCallback(async () => {
     const data = await getAllCasesByPublishDate()
     let dataByYear = groupBy(data, el => el.date.getFullYear())
-    setCasesByDate(dataByYear[year])
+    const casesByMonth = Object.values( groupBy(dataByYear[year], el => el.date.getMonth() + 1) )
+        .map( el => el.reduce((acc, el) => acc + el.cases, 0))
+        .map( (el, index) => ({ month: `Month: ${index  + 1}`, cases: el }))
+    setCasesByDate(casesByMonth)
   }, [])
 
   useEffect(() => {
@@ -22,7 +24,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        {casesByDate.length > 0 &&
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart
+                width={500}
+                height={300}
+                data={casesByDate}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+            >
+              <CartesianGrid strokeDasharray="3 3"/>
+              <XAxis dataKey="month"/>
+              <YAxis/>
+              <Tooltip/>
+              <Legend/>
+              <Bar dataKey="cases" fill="#8884d8"/>
+            </BarChart>
+          </ResponsiveContainer>
+        }
       </header>
     </div>
   );
